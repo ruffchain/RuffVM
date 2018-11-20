720ae099d150a39ac3ed51f99aa42475ff358da1
Heyong Nie <nieheyong@nanchao.org>
2018-11-20 17:36:56 +0800

    add chain style vm api

--- index.js
+++ index.js
@@ -1,3 +1,63 @@
-var bindings = require("bindings")("ruff-vm.node");
+var bindings = require('bindings')('ruff-vm.node')
 
-module.exports.run = bindings.run;
+module.exports.run = bindings.run
+const vm = require('bindings')('ruff-vm.node')
+
+function _stringToU8Buf(str) {
+  let b = Buffer.from(str)
+  b = b.buffer.slice(b.byteOffset, b.byteOffset + b.byteLength)
+  const U8Buf = new Uint8Array(b, 0, b.byteLength)
+  return U8Buf
+}
+
+let isVmBusy = false
+class Script {
+  constructor(code = null) {
+    this._script = code ? _stringToU8Buf(`${code}`) : null
+    this._option = null
+    this._sandbox = null
+    this._userCode = null
+  }
+
+  setSandbox(sandbox) {
+    this._sandbox = sandbox
+    return this
+  }
+
+  setOption(option) {
+    this._option = option
+    return this
+  }
+
+  setUserCode(str) {
+    this._userCode = str
+    return this
+  }
+
+  runAsync() {
+    if (isVmBusy) {
+      throw new Error('vm is busy now')
+    }
+    isVmBusy = true
+
+    const { _userCode, _script, _sandbox, _option } = this
+    return new Promise((resolve, reject) => {
+      vm.run(_userCode, _script, _sandbox, _option, (err, res) => {
+        setImmediate(() => {
+          err ? reject(err) : resolve(res)
+          isVmBusy = false
+        })
+      })
+    })
+  }
+}
+
+function createScript(code) {
+  return new Script(code)
+}
+
+module.exports = {
+  run: vm.run,
+  Script,
+  createScript
+}
---
+++ test/script.test.js
@@ -0,0 +1,247 @@
+const assert = require('assert')
+const { Script, createScript } = require('../index.js')
+
+function bufferToArrayBuffer(b) {
+  return b.buffer.slice(b.byteOffset, b.byteOffset + b.byteLength)
+}
+
+describe('ruff vm basic', function() {
+  describe('export', function() {
+    it('should export Script', function() {
+      assert(typeof Script === 'function')
+    })
+  })
+
+  it('function createScript could creat a script', async () => {
+    const script = createScript()
+    assert(script instanceof Script)
+  })
+
+  describe('run', function() {
+    it('should run simple script with empty context', async () => {
+      const res = await createScript()
+        .setUserCode('({ret: "HI"})')
+        .runAsync()
+      assert(res === '{"ret":"HI"}')
+    })
+  })
+
+  it('should twice run simple script with empty context', async () => {
+    const script = createScript()
+    const res1 = await script.setUserCode('({ret: "HI"})').runAsync()
+    assert(res1 === '{"ret":"HI"}')
+    const res2 = await script.setUserCode('({ret: "HELLO"})').runAsync()
+    assert(res2 === '{"ret":"HELLO"}')
+  })
+
+  it('should exit run infinite loop when set cpu limit', async () => {
+    const res = await createScript()
+      .setUserCode('while(1){};')
+      .setOption({ cpuCount: 200, memSizeKB: 256 })
+      .runAsync()
+    assert(res === '{"error": "Abort script"}')
+  })
+
+  it('should get value in script from context', async () => {
+    var code = 'var ruff = {value: "hello"};'
+    const res = await createScript(code)
+      .setUserCode('({ "ret": ruff.value })')
+      .runAsync()
+    assert(res === '{"ret":"hello"}')
+  })
+})
+
+describe('vm resolve', function() {
+  it('should resolve with ArrayBuffer', async () => {
+    let isTriggered = false
+
+    function hostTest(vmResolve, param) {
+      return new Promise(function(resolve) {
+        isTriggered = true
+        let ab1 = bufferToArrayBuffer(Buffer.from(`this is hostapi ${param}`))
+        const u8 = new Uint8Array(ab1, 0, ab1.byteLength)
+        setTimeout(() => {
+          resolve(vmResolve(u8))
+        }, 100)
+      })
+    }
+    let apiTable = {
+      test: hostTest
+    }
+
+    const res = await createScript()
+      .setUserCode('test("test");')
+      .setSandbox(apiTable)
+      .runAsync()
+
+    assert(isTriggered)
+    let expectBuffer = bufferToArrayBuffer(Buffer.from('this is hostapi test'))
+    let expectU8 = new Uint8Array(expectBuffer, 0, expectBuffer.byteLength)
+    let retU8 = new Uint8Array(res, 0, res.byteLength)
+    assert.deepStrictEqual(retU8, expectU8)
+  })
+})
+
+describe('bridge VM and Host', function() {
+  it('Host could get string from VM', async () => {
+    let isTriggered = false
+    function hostTest(vmResolve, param) {
+      assert(param === 'hi from ruffvm')
+      isTriggered = true
+      return true
+    }
+
+    let apiTable = {
+      test: hostTest
+    }
+
+    await createScript()
+      .setUserCode('test("hi from ruffvm");')
+      .setSandbox(apiTable)
+      .runAsync()
+
+    assert(isTriggered)
+  })
+
+  it('Host could get utf8 string from VM', async () => {
+    let isTriggered = false
+    function hostTest(vmResolve, param) {
+      assert(param === 1)
+      isTriggered = true
+      return true
+    }
+
+    let apiTable = {
+      test: hostTest
+    }
+
+    await createScript()
+      .setUserCode('test(1);')
+      .setSandbox(apiTable)
+      .runAsync()
+
+    assert(isTriggered)
+  })
+
+  it('Host could get number from VM', async () => {
+    let isTriggered = false
+    function hostTest(vmResolve, param) {
+      assert(param === 2.5)
+      isTriggered = true
+    }
+
+    let apiTable = {
+      test: hostTest
+    }
+
+    await createScript()
+      .setUserCode('test(2.5);')
+      .setSandbox(apiTable)
+      .runAsync()
+
+    assert(isTriggered)
+  })
+
+  it('VM could get number from Host', async () => {
+    let isTriggered = false
+    function hostTest(vmResolve, param) {
+      isTriggered = true
+      return 3.6
+    }
+
+    let apiTable = {
+      test: hostTest
+    }
+
+    await createScript()
+      .setUserCode('var ret = test(); if (ret === 3.6) {ret} else {while(1);}')
+      .setSandbox(apiTable)
+      .runAsync()
+
+    assert(isTriggered)
+  })
+
+  it('Host could get arraybuffer from VM', async () => {
+    let isTriggered = false
+    function hostTest(vmResolve, param) {
+      isTriggered = true
+      const u8 = new Uint8Array(param, 0, param.byteLength)
+      assert(u8.length === 20)
+      assert((u8[0] = 12 && u8[1] === 234))
+    }
+
+    let apiTable = {
+      test: hostTest
+    }
+
+    const script = 'var buf = new Uint8Array(20); buf[0] = 12;buf[1]=234;test(buf.buffer);'
+    await createScript()
+      .setUserCode(script)
+      .setSandbox(apiTable)
+      .runAsync()
+
+    assert(isTriggered)
+  })
+
+  it('Host could get Bool from VM', async () => {
+    let script = 'false;'
+    const res = await createScript()
+      .setUserCode(script)
+      .runAsync()
+    assert(res === false)
+  })
+
+  it('VM could get int number from Host', async () => {
+    let isTriggered = false
+    function hostTest(vmResolve, param) {
+      isTriggered = true
+      return 3
+    }
+
+    let apiTable = {
+      test: hostTest
+    }
+    await createScript()
+      .setUserCode('var ret = test(); if (ret === 3) {ret} else {while(1);}')
+      .setSandbox(apiTable)
+      .runAsync()
+
+    assert(isTriggered)
+  })
+})
+
+describe('Context', function() {
+  it('could define function in context', async () => {
+    let isTriggered = false
+
+    function apiFunction(resolve, param) {
+      isTriggered = true
+      const u8 = new Uint8Array(param, 0, param.byteLength)
+      assert(u8.length === 20)
+      assert((u8[0] = 1 && u8[1] === 2))
+      return true
+    }
+
+    var code = `
+      function helloFun(parameterString) {
+        var buf = new Uint8Array(20)
+        buf[0] = 1
+        buf[1] = 2
+        return hello(buf.buffer)
+      }
+    `
+
+    var apiObject = {
+      hello: apiFunction
+    }
+
+    const res = await createScript(code)
+      .setUserCode('helloFun("ruffVM")')
+      .setSandbox(apiObject)
+      .setOption({ cpuCount: 1, memSizeKB: 200 })
+      .runAsync()
+
+    assert(isTriggered)
+    assert(res === true)
+  })
+})
