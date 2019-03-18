dbb2597c66fdc908495dc69aa928cd2322dd78d4
Jie Yang <jie.yang@nanchao.org>
2019-03-18 16:28:18 +0800

    Fix typo

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ export class Script {
     setSandbox(sandbox: any): this;
     setOption(option: any): this;
     setUserCode(str: string): this;
-    runAsyn(): Promise<void>;
+    runAsync(): Promise<void>;
 }
 
 export function createScript(type: string): Script
